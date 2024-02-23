ed2ee5e4d5eb035f7915837621bab01b370cf8eb
oksid <oksid123123@gmail.com>
2024-02-23 14:31:29 +0400

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿var stage = new Konva.Stage({
+    container: 'konva-container',
+    width: window.innerWidth,
+    height: window.innerHeight,
+    draggable: true,
+    fill: 'black',
+});
+
+var layer = new Konva.Layer();
+
+stage.add(layer);
+
+var rect = new Konva.Rect({
+    x: 50,
+    y: 50,
+    width: 100,
+    height: 50,
+    fill: 'blue',
+});
+
+layer.add(rect);
+
+layer.draw();
